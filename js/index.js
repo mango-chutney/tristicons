b3b1b8035cdf1575e96f4a37da464742a1ab366d
Francisco Martinez <francis@mangochutney.com.au>
2017-11-06 14:36:21 +1030

    ✨ export the json as default

---
+++ js/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+Object.defineProperty(exports, '__esModule', {
+  value: true,
+});
+
+var tristicons = require('./icons.json');
+
+function _interopRequireDefault(obj) {
+  return obj && obj.__esModule ? obj : { default: obj };
+}
+
+exports.default = tristicons;
